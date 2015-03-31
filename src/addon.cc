8c27e273b29fe6a680eabb8963cfdfd1ba2c92d7
unbornchikken <gabor.mezo@outlook.com>
2015-03-31 21:58:42 +0200

    initial commit

---
+++ src/addon.cc
@@ -0,0 +1,28 @@
+/*********************************************************************
+ * NAN - Native Abstractions for Node.js
+ *
+ * Copyright (c) 2015 NAN contributors
+ *
+ * MIT License <https://github.com/rvagg/nan/blob/master/LICENSE.md>
+ ********************************************************************/
+
+#include <nan.h>
+#include "sync.h"   // NOLINT(build/include)
+#include "async.h"  // NOLINT(build/include)
+
+using v8::FunctionTemplate;
+using v8::Handle;
+using v8::Object;
+using v8::String;
+
+// Expose synchronous and asynchronous access to our
+// Estimate() function
+void InitAll(Handle<Object> exports) {
+  exports->Set(NanNew<String>("calculateSync"),
+    NanNew<FunctionTemplate>(CalculateSync)->GetFunction());
+
+  exports->Set(NanNew<String>("calculateAsync"),
+    NanNew<FunctionTemplate>(CalculateAsync)->GetFunction());
+}
+
+NODE_MODULE(addon, InitAll)
---
+++ src/async.cc
@@ -0,0 +1,61 @@
+/*********************************************************************
+ * NAN - Native Abstractions for Node.js
+ *
+ * Copyright (c) 2015 NAN contributors
+ *
+ * MIT License <https://github.com/rvagg/nan/blob/master/LICENSE.md>
+ ********************************************************************/
+
+#include <nan.h>
+#include "pi_est.h"  // NOLINT(build/include)
+#include "async.h"  // NOLINT(build/include)
+
+using v8::Function;
+using v8::Local;
+using v8::Null;
+using v8::Number;
+using v8::Value;
+
+class PiWorker : public NanAsyncWorker {
+ public:
+  PiWorker(NanCallback *callback, int points)
+    : NanAsyncWorker(callback), points(points), estimate(0) {}
+  ~PiWorker() {}
+
+  // Executed inside the worker-thread.
+  // It is not safe to access V8, or V8 data structures
+  // here, so everything we need for input and output
+  // should go on `this`.
+  void Execute () {
+    estimate = Estimate(points);
+  }
+
+  // Executed when the async work is complete
+  // this function will be run inside the main event loop
+  // so it is safe to use V8 again
+  void HandleOKCallback () {
+    NanScope();
+
+    Local<Value> argv[] = {
+        NanNull()
+      , NanNew<Number>(estimate)
+    };
+
+    callback->Call(2, argv);
+  }
+
+ private:
+  int points;
+  double estimate;
+};
+
+// Asynchronous access to the `Estimate()` function
+NAN_METHOD(CalculateAsync) {
+  NanScope();
+
+  int points = args[0]->Uint32Value();
+  NanCallback *callback = new NanCallback(args[1].As<Function>());
+
+  NanAsyncQueueWorker(new PiWorker(callback, points));
+  NanReturnUndefined();
+}
